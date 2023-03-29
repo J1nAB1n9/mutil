1241c85b5d96cf8be9a85687174695e46085937b
J1nAB1n9 <415086234@qq.com>
2023-03-29 22:47:19 +0800

    Group核心功能提交

---
+++ jache/Group.go
@@ -0,0 +1,73 @@
+package jache
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+type Group struct {
+	name     string
+	getter   Getter
+	mainCahe cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:     name,
+		getter:   getter,
+		mainCahe: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	defer mu.RUnlock()
+	g := groups[name]
+
+	return g
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is nil")
+	}
+
+	if v, ok := g.mainCahe.get(key); ok {
+		log.Println("[GeeCache] hit")
+		return v, nil
+	}
+
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCahe.add(key, value)
+}
